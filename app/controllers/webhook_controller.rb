e79531c0255ab6bdc7cf3a842b30051ac242ac5e
Alexander Standke <xanderstrike@gmail.com>
2017-10-13 23:01:28 -0700

    add support for now playing

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,16 +3,15 @@ class WebhookController < ApplicationController
 
   def index
     payload = JSON.parse(params['payload'])
-    return unless payload['event'] == 'media.scrobble'
 
     access_token = AccessToken.where(uid: params[:uid]).first
     return unless payload['Account']['title'].downcase == access_token.username.downcase
 
     case payload['Metadata']['librarySectionType']
     when 'show'
-      TraktService.scrobble_episode(payload, access_token)
+      TraktService.submit_episode(payload['event'].gsub('media.', ''), payload, access_token)
     when 'movie'
-      TraktService.scrobble_movie(payload, access_token)
+      TraktService.submit_movie(payload['event'].gsub('media.', ''), payload, access_token)
     end
   end
 end
--- app/services/trakt_service.rb
+++ app/services/trakt_service.rb
@@ -5,6 +5,22 @@ class TraktService
   CLIENT_ID = ENV['CLIENT_ID']
   CLIENT_SECRET = ENV['CLIENT_SECRET']
 
+  ROUTE_DICT = {
+    'play' => 'start',
+    'pause' => 'pause',
+    'resume' => 'start',
+    'stop' => 'stop',
+    'scrobble' => 'stop'
+  }.freeze
+
+  PROGRESS_DICT = {
+    'play' => 0.0,
+    'pause' => nil,
+    'resume' => nil,
+    'stop' => nil,
+    'scrobble' => 90.0
+  }.freeze
+
   class << self
     def build_auth_link(redirect_uri)
       "https://trakt.tv/oauth/authorize?client_id=#{CLIENT_ID}&redirect_uri=#{URI.encode(redirect_uri)}%3Fusername=USERNAME&response_type=code"
@@ -27,35 +43,35 @@ class TraktService
       JSON.parse(response)
     end
 
-    def scrobble_movie(info, access_token)
+    def submit_movie(action, info, access_token)
       values = {
         movie: find_movie(info['Metadata']['title'], info['Metadata']['year']),
-        progress: 90.0,
+        progress: PROGRESS_DICT[action],
         app_version: '1.0',
         app_date: '2017-09-22'
       }
 
-      headers = {
-        content_type: 'application/json',
-        authorization: "Bearer #{access_token.access_token}",
-        trakt_api_version: '2',
-        trakt_api_key: CLIENT_ID
-      }
+      puts values
 
-      response = RestClient.post 'https://api.trakt.tv/scrobble/stop', JSON.generate(values), headers
-      JSON.parse(response)
+      send_scrobble(ROUTE_DICT[action], values, access_token)
     end
 
-    def scrobble_episode(info, access_token)
-      guid = info['Metadata']['guid'].match(/thetvdb:\/\/(\d*)\/(\d*)\/(\d)*/)
+    def submit_episode(action, info, access_token)
+      guid = info['Metadata']['guid'].match(/thetvdb:\/\/(\d*)\/(\d*)\/(\d*)/)
 
       values = {
         episode: find_episode(guid[1].to_i, guid[2].to_i, guid[3].to_i),
-        progress: 90.0,
+        progress: PROGRESS_DICT[action],
         app_version: '1.0',
         app_date: '2017-09-22'
       }
 
+      send_scrobble(ROUTE_DICT[action], values, access_token)
+    end
+
+    private
+
+    def send_scrobble(route, values, access_token)
       headers = {
         content_type: 'application/json',
         authorization: "Bearer #{access_token.access_token}",
@@ -63,11 +79,9 @@ class TraktService
         trakt_api_key: CLIENT_ID
       }
 
-      response = RestClient.post 'https://api.trakt.tv/scrobble/stop', JSON.generate(values), headers
+      response = RestClient.post "https://api.trakt.tv/scrobble/#{route}", JSON.generate(values), headers
       JSON.parse(response)
     end
-
-    # private
 
     def find_episode(id, season, episode)
       headers = {
